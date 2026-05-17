ea29b0f904cea68765f645128bbc78e04a1959bc
Luci <luci.vilches@duocuc.cl>
2026-05-17 18:17:02 -0400

    Delete Sabores_Globales_Usuarios/Sabores_Globales_Usuarios directory

--- Sabores_Globales_Usuarios/Sabores_Globales_Usuarios/src/main/java/com/saboresGlobales/usuarios/Sabores_Globales_Usuarios/Repository/RolRepository.java
+++
@@ -1,9 +0,0 @@
-package com.saboresGlobales.usuarios.Sabores_Globales_Usuarios.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.saboresGlobales.usuarios.Sabores_Globales_Usuarios.Modelo.Rol;
-
-public interface RolRepository extends JpaRepository<Rol,Long> {
-
-}
