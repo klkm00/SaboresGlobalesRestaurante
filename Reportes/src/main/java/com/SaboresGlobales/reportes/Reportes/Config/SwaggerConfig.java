a7e3912d38b5e12b00661a49a993883fcee77efc
Luci <luci.vilches@duocuc.cl>
2026-06-06 20:22:14 -0400

    Swagger Implementado
    swagger Listo

---
+++ Reportes/src/main/java/com/SaboresGlobales/reportes/Reportes/Config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.SaboresGlobales.reportes.Reportes.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Reportes API")
+                        .version("1.0")
+                        .description("API para la gestión de reportes en Sabores Globales"));
+    }
+}
