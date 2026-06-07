4d1ac4b6fc06df2f71f47f7f2c267478d89417a7
Luci <luci.vilches@duocuc.cl>
2026-06-06 20:29:44 -0400

    Delete ms-inventario directory

--- ms-inventario/src/main/java/com/saboresglobales/inventario/repository/InventarioRepository.java
+++
@@ -1,5 +0,0 @@
-
-package com.saboresglobales.inventario.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.saboresglobales.inventario.model.Inventario;
-public interface InventarioRepository extends JpaRepository<Inventario, Long> {}
