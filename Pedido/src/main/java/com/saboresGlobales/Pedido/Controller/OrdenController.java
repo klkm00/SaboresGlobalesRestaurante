77992a8d410df21bebcc3a33e01dc1eb5c93c98b
Luci <luci.vilches@duocuc.cl>
2026-06-06 20:25:32 -0400

    Swagger Implementado
    Implementacion de Swagger, todo listo

--- Pedido/src/main/java/com/saboresGlobales/Pedido/Controller/OrdenController.java
+++ Pedido/src/main/java/com/saboresGlobales/Pedido/Controller/OrdenController.java
@@ -18,20 +18,25 @@ import com.saboresGlobales.Pedido.DTO.OrdenRequestDTO;
 import com.saboresGlobales.Pedido.DTO.OrdenResponseDTO;
 import com.saboresGlobales.Pedido.Service.OrdenService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/v1/ordenes")
 @RequiredArgsConstructor
+@Tag(name = "Ordenes", description = "Operaciones relacionadas con las órdenes de pedido")
 public class OrdenController {
     private final OrdenService ordenService;
 
     @GetMapping
+    @Operation(summary = "Obtener todas las órdenes", description = "Devuelve una lista de todas las órdenes de pedido")
     public ResponseEntity<List<OrdenResponseDTO>> obtenerTodos() {
         return ResponseEntity.ok(ordenService.obtenerTodos());
          
     }
     @GetMapping("/{id}")
+    @Operation(summary = "Obtener una orden por ID", description = "Devuelve los detalles de una orden de pedido específica por su ID")
         public ResponseEntity<OrdenResponseDTO> obtenerporId(@PathVariable Long id){
             return ordenService.obtenerPorId(id)
             .map(ResponseEntity::ok)
@@ -41,12 +46,14 @@ public class OrdenController {
 
         
     @PostMapping
+    @Operation(summary = "Crear una nueva orden", description = "Crea una nueva orden de pedido con los datos proporcionados")
     public ResponseEntity<OrdenResponseDTO> crear(@RequestBody OrdenRequestDTO dto) {
         OrdenResponseDTO nuevaOrden = ordenService.guardar(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(nuevaOrden);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Actualizar una orden", description = "Actualiza los datos de una orden de pedido específica por su ID")
     public ResponseEntity<OrdenResponseDTO> actualizar(@PathVariable Long id,
          @RequestBody OrdenResponseDTO dto) {
        
@@ -56,6 +63,7 @@ public class OrdenController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Eliminar una orden", description = "Elimina una orden de pedido específica por su ID")
     public ResponseEntity<Void> eliminar(@PathVariable Long id) {
         ordenService.eliminar(id);
         return ResponseEntity.noContent().build();
--- Pedido/src/main/java/com/saboresGlobales/Pedido/Controller/PedidoController.java
+++ Pedido/src/main/java/com/saboresGlobales/Pedido/Controller/PedidoController.java
@@ -17,6 +17,8 @@ import com.saboresGlobales.Pedido.DTO.PedidoRequestDTO;
 import com.saboresGlobales.Pedido.DTO.PedidoResponseDTO;
 import com.saboresGlobales.Pedido.Service.PedidoService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,24 +27,27 @@ import org.springframework.web.bind.annotation.PostMapping;
 @RestController
 @RequestMapping("/api/v1/pedidos")
 @RequiredArgsConstructor
+@Tag(name = "Pedidos", description = "Operaciones relacionadas con los pedidos")
 public class PedidoController {
 
     private final PedidoService pedidoService;
     @GetMapping
-
+    @Operation(summary = "Obtener todos los pedidos", description = "Devuelve una lista de todos los pedidos registrados en el sistema")
     public ResponseEntity<List<PedidoResponseDTO>> obtenerTodos() {
         return ResponseEntity.ok(pedidoService.obtenerTodos());
          
     }
 
     @GetMapping("/{id}")
+        @Operation(summary = "Obtener un pedido por ID", description = "Devuelve los detalles de un pedido específico por su ID")
         public ResponseEntity<PedidoResponseDTO> obtenerporId(@PathVariable Long id){
             return pedidoService.obtenerPorId(id)
             .map(ResponseEntity::ok)
             .orElse(ResponseEntity.notFound().build());
         }
 
-        @PostMapping
+    @PostMapping
+            @Operation(summary = "Crear un nuevo pedido", description = "Crea un nuevo pedido con los datos proporcionados")
         public ResponseEntity<PedidoResponseDTO>create(@Valid @RequestBody PedidoRequestDTO dto) {
             return ResponseEntity.status(201).body(pedidoService.guardar(dto));
         }
@@ -52,6 +57,7 @@ public class PedidoController {
 
 
     @PutMapping("/{id}")
+    @Operation(summary = "Actualizar un pedido", description = "Actualiza los datos de un pedido específico por su ID")
     public ResponseEntity<PedidoResponseDTO> actualizar(@PathVariable Long id,
         @Valid @RequestBody PedidoRequestDTO dto) {
             return pedidoService.actualizar(id, dto)
@@ -61,6 +67,7 @@ public class PedidoController {
     
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Eliminar un pedido", description = "Elimina un pedido específico por su ID")
     public ResponseEntity<Void> eliminar(@PathVariable Long id){
         if(pedidoService.obtenerPorId(id).isEmpty()){
             return ResponseEntity.notFound().build();
@@ -70,6 +77,7 @@ public class PedidoController {
         return ResponseEntity.noContent().build();
     }    
     @GetMapping("/buscar")
+    @Operation(summary = "Buscar pedidos por orden", description = "Devuelve una lista de pedidos asociados a una orden específica por su ID")
     public ResponseEntity<List<PedidoResponseDTO>> buscarPorOrden(
         @RequestParam Long orden){
             return ResponseEntity.ok(pedidoService.buscarPorOrden(orden));
