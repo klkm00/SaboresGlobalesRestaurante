e1731b926eba2b3e38f151d19ea3c322c3d1182b
Luci <luci.vilches@duocuc.cl>
2026-06-06 20:29:01 -0400

    Delete ms_productos directory

--- ms_productos/src/main/java/com/example/productos/repository/ProductoRepository.java
+++
@@ -1,8 +0,0 @@
-
-package com.example.productos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.productos.model.Producto;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-}
