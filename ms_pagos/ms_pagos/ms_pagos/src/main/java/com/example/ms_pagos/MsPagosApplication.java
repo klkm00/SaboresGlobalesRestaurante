350b855b247fc0899e34ec620d1f7ceea7530021
Luci <luci.vilches@duocuc.cl>
2026-06-19 14:01:01 -0400

    yml implementado

--- ms_pagos/ms_pagos/ms_pagos/src/main/java/com/example/ms_pagos/MsPagosApplication.java
+++ ms_pagos/ms_pagos/ms_pagos/src/main/java/com/example/ms_pagos/MsPagosApplication.java
@@ -2,8 +2,9 @@ package com.example.ms_pagos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class MsPagosApplication {
 
 	public static void main(String[] args) {
