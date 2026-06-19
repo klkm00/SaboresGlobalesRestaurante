9913373ea195fa9e066ae377ca65cd394281abd0
Luci <luci.vilches@duocuc.cl>
2026-06-19 13:57:14 -0400

    yml implmentado

--- Pedido/src/main/java/com/saboresGlobales/Pedido/PedidoApplication.java
+++ Pedido/src/main/java/com/saboresGlobales/Pedido/PedidoApplication.java
@@ -2,8 +2,9 @@ package com.saboresGlobales.Pedido;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class PedidoApplication {
 
 	public static void main(String[] args) {
