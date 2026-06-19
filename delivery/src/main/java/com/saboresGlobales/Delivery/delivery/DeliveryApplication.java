92480f6f1915ae5625269b91e8e6f8b3e85a6189
Luci <luci.vilches@duocuc.cl>
2026-06-19 14:07:46 -0400

    YML implementado

--- delivery/src/main/java/com/saboresGlobales/Delivery/delivery/DeliveryApplication.java
+++ delivery/src/main/java/com/saboresGlobales/Delivery/delivery/DeliveryApplication.java
@@ -2,8 +2,9 @@ package com.saboresGlobales.Delivery.delivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class DeliveryApplication {
 
 	public static void main(String[] args) {
